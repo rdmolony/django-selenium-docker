6a38d488c67a8ae638ccba5cd8f278c1960027a9
Rowan Molony <rowan.molony@mainstreamrp.com>
2021-11-25 14:47:46 +0000

    Link Django server to Selenium driver
    Enables Selenium to attempt a login to the Django
    server.  This currently fails due to a database access
    issue

---
+++ tests/test_login.py
@@ -0,0 +1,56 @@
+import socket
+from urllib.parse import urlparse
+
+from django.db import connection
+import pytest
+from pytest_django.live_server_helper import LiveServer
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+@pytest.fixture(autouse=True)
+def override_allowed_hosts(settings):
+    settings.ALLOWED_HOSTS = ["*"]  # Disable ALLOW_HOSTS
+
+
+@pytest.fixture
+def live_server_at_host():
+    # Set host to externally accessible web server address
+    host = socket.gethostbyname(socket.gethostname())
+    return LiveServer(addr=host)
+
+
+@pytest.fixture
+def driver_init():
+    remote_driver = webdriver.Remote(
+        command_executor='http://selenium:4444/wd/hub',
+        desired_capabilities=DesiredCapabilities.CHROME,
+    )
+    remote_driver.implicitly_wait(5)
+    yield remote_driver
+    remote_driver.close()
+
+
+@pytest.mark.django_db(transaction=True)
+def test_user_login(live_server_at_host, driver_init, django_user_model):
+    """
+    As a superuser with valid credentials, I should gain
+    access to the Django admin.
+    """
+    driver = driver_init
+    live_server_url = str(live_server_at_host)
+    user = django_user_model.objects.create(username="george", password="password1234")
+    user.save()
+
+    driver.get(live_server_url)
+    username_input = driver.find_element_by_name('username')
+    username_input.send_keys('george')
+    password_input = driver.find_element_by_name('password')
+    password_input.send_keys('password1234')
+    driver.find_element_by_xpath('//input[@value="Log in"]').click()
+
+    path = urlparse(driver.current_url).path
+    assert path == '/'
+
+    body_text = driver.find_element_by_tag_name('body').text
+    assert 'WELCOME, GEORGE.' in body_text
