87dd83093442e202de5dfdb1f0be09b3f5a2e16c
Rowan Molony <rowan.molony@mainstreamrp.com>
2021-12-10 15:38:03 +0000

    Replace browser close with browser quit & clean
    Browser close leaves a hanging browser and so selenium has to be
    restarted between each test run
    
    Also cleaned up variable naming and type hinting

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,12 @@
-import socket
+from importlib import import_module
 from urllib.parse import urlparse
 
-from django.db import connection
+from django.conf import settings
+from django.conf import Settings
+from django.contrib.auth import SESSION_KEY, BACKEND_SESSION_KEY, HASH_SESSION_KEY
+from django.contrib.auth.models import User
+from django.test.client import Client
+from django.contrib.sessions.backends.db import SessionStore 
 import pytest
 from pytest_django.live_server_helper import LiveServer
 from selenium import webdriver
@@ -9,46 +14,44 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 @pytest.fixture(autouse=True)
-def override_allowed_hosts(settings):
+def override_allowed_hosts(settings: Settings) -> None:
     settings.ALLOWED_HOSTS = ["*"]  # Disable ALLOW_HOSTS
 
 
 @pytest.fixture
-def live_server_at_host():
+def live_server_url() -> str:
     # Set host to externally accessible web server address
-    host = socket.gethostbyname(socket.gethostname())
-    return LiveServer(addr=host)
+    return str(LiveServer(addr="django"))
 
 
 @pytest.fixture
-def driver_init():
-    remote_driver = webdriver.Remote(
+def browser() -> webdriver.Remote:
+    browser_ = webdriver.Remote(
         command_executor='http://selenium:4444/wd/hub',
         desired_capabilities=DesiredCapabilities.CHROME,
     )
-    remote_driver.implicitly_wait(5)
-    yield remote_driver
-    remote_driver.close()
+    yield browser_
+    browser_.quit()
 
 
-@pytest.mark.django_db(transaction=True)
-def test_admin_user_login(live_server_at_host, driver_init, admin_user):
+@pytest.mark.django_db
+def test_manual_admin_user_login(
+    live_server_url: str, browser: webdriver.Remote, admin_user: User
+) -> None:
     """
     As a superuser with valid credentials, I should gain
     access to the Django admin.
     """
-    driver = driver_init
-    live_server_url = str(live_server_at_host)
+    browser.get(live_server_url)
 
-    driver.get(live_server_url)
-    username_input = driver.find_element_by_name('username')
+    username_input = browser.find_element_by_name('username')
     username_input.send_keys('admin')
-    password_input = driver.find_element_by_name('password')
+    password_input = browser.find_element_by_name('password')
     password_input.send_keys('password')
-    driver.find_element_by_xpath('//input[@value="Log in"]').click()
+    browser.find_element_by_xpath('//input[@value="Log in"]').click()
 
-    path = urlparse(driver.current_url).path
+    path = urlparse(browser.current_url).path
     assert path == '/'
 
-    body_text = driver.find_element_by_tag_name('body').text
+    body_text = browser.find_element_by_tag_name('body').text
     assert 'WELCOME, ADMIN.' in body_text
